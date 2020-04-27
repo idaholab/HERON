249980644bd742ca972ba4dc130e7b141e27821a
Abhinav Gairola <abhinav>
2020-04-27 16:29:50 -0400

    add os.path.join in clean

--- doc/clean.py
+++ doc/clean.py
@@ -2,7 +2,7 @@ import os
 import shutil as sh
 import glob
 path=os.path.dirname(os.path.abspath(__file__))
-file_to_remove=glob.glob(path+'/pdf'+'/*')
+file_to_remove=glob.glob(os.path.join(path,'pdf','*'))
 for file in file_to_remove:
   if file != path+"/pdf"+"/HERON_user_manual.pdf":
     os.remove(file)

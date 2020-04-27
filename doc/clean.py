80256be8d61aee6da874f762e427106120081a6f
Abhinav Gairola <abhinav>
2020-04-27 16:15:15 -0400

    All

--- doc/clean.py
+++ doc/clean.py
@@ -4,8 +4,5 @@ import glob
 path=os.path.dirname(os.path.abspath(__file__))
 file_to_remove=glob.glob(path+'/pdf'+'/*')
 for file in file_to_remove:
-  if file != path+"/pdf"+"/pdfHERON_user_manual.pdf":
-    if file == path+"/pdf"+"/pics":
-      sh.rmtree(path+"/pdf"+"/pics")
-    elif file != path+"/pdf"+"/pics":
-      os.remove(file)
+  if file != path+"/pdf"+"/HERON_user_manual.pdf":
+    os.remove(file)

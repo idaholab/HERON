ae0eb92dfb325b5328e455c6ed0227a609082d93
Abhinav Gairola <abhinav>
2020-05-01 13:59:09 -0400

    some cleaning

--- doc/clean.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil as sh
-import glob
-path=os.path.dirname(os.path.abspath(__file__))
-file_to_remove=glob.glob(os.path.join(path,'pdf','*'))
-for file in file_to_remove:
-  if file != path+"/pdf"+"/HERON_user_manual.pdf":
-    os.remove(file)

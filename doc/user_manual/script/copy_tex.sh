c33133e484a222cd1cf93242c8301ebeacebe6cd
Paul W. Talbot <paul.talbot@inl.gov>
2020-05-06 10:52:35 -0600

    reorg and fix for autodoc

---
+++ doc/user_manual/script/copy_tex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT_NAME=`readlink $0`
+if test -x "$SCRIPT_NAME";
+then
+    SCRIPT_DIRNAME=`dirname $SCRIPT_NAME`
+else
+    SCRIPT_DIRNAME=`dirname $0`
+fi
+SCRIPT_DIR=`(cd $SCRIPT_DIRNAME; pwd)`
+cd $SCRIPT_DIR
+if [[ ! -d "../build" ]]; then mkdir "../build"; fi
+if [[ ! -d "../pdf" ]]; then mkdir "../pdf"; fi
+cp ../src/HERON_user_manual.tex ../build/
+cp ../src/HERON_user_manual.bib ../build/

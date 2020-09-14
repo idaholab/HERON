aa1fcf7a8c690fbb2662a0b4c275501cb642c64a
abhinav <abhinav.gairola@inl.gov>
2020-09-14 15:55:10 -0400

    Origin

--- tests/integration_tests/production_flex_opt/Opt_Runs_o/optimize/7/transfers.py
+++ tests/integration_tests/production_flex_opt/Opt_Runs_o/optimize/7/transfers.py
@@ -16,22 +16,14 @@ def electric_consume(data, meta):
   # model = meta['HERON']['pyomo_model']
   # component = meta['HERON']['component']
   activity = meta['HERON']['activity']
-  #print("This is activity", activity, activity.keys(),activity['electricity'])
-  #aaa
-  #print("This is activity", activity, activity.keys())
   # TODO a get_activity method for the dispatcher -> returns object-safe activity (expression or value)?
   amount = -1 * activity['electricity']
   data = {'driver': amount}
   return data, meta
 
 def flex_price(data, meta):
-  #print(meta['HERON'].keys())
-  #print("This is the activity", meta['HERON']['activity']['electricity'])
-  #aaa
   sine = meta['HERON']['RAVEN_vars']['Signal']
   t = meta['HERON']['time_index']
-  #print("This is signal", sine)
-  #aaa
   # DispatchManager
   # scale electricity consumed to flex between -1 and 1
   amount = - 2 * (sine[t] - 0.5)

494de6888e7aa9fc49118e41a6652e0b34e7cf22
Paul W. Talbot <paul.talbot@inl.gov>
2020-09-23 09:13:42 -0600

    cleanup

--- tests/integration_tests/validator/transfers.py
+++ tests/integration_tests/validator/transfers.py
@@ -6,22 +6,26 @@
 """
 
 def electric_consume(data, meta):
-  ## works with generic?
-  # activity = meta['raven_vars']['HERON_pyomo_model']
-  # t = meta['t']
-  # flip sign because we consume the electricity
-  # E = -1.0 * activity['electricity'][t]
-
-  ## works with pyomo
-  # model = meta['HERON']['pyomo_model']
-  # component = meta['HERON']['component']
+  """
+    Transfer function to change sign of electricity consumed, for cashflows
+    @ In, data, dict, partial requested activity
+    @ In, meta, dict, additional info
+    @ Out, data, dict, filled-in requested activity
+    @ Out, meta, dict, additional info (possibly modified, possibly not)
+  """
   activity = meta['HERON']['activity']
-  # TODO a get_activity method for the dispatcher -> returns object-safe activity (expression or value)?
   amount = -1 * activity['electricity']
   data = {'driver': amount}
   return data, meta
 
 def flex_price(data, meta):
+  """
+    Transfer function to acquire the flexible price of electricity
+    @ In, data, dict, partial requested activity
+    @ In, meta, dict, additional info
+    @ Out, data, dict, filled-in requested activity
+    @ Out, meta, dict, additional info (possibly modified, possibly not)
+  """
   sine = meta['HERON']['RAVEN_vars']['Signal']
   t = meta['HERON']['time_index']
   # DispatchManager

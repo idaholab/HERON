13d0202d99b980fb82694c0076471de9f6a441e9
Paul W. Talbot <paul.talbot@inl.gov>
2020-10-15 14:14:55 -0600

    still whitespace maybe

--- tests/integration_tests/min_demand/transfers.py
+++ tests/integration_tests/min_demand/transfers.py
@@ -3,9 +3,14 @@
 """
   Implements transfer functions
 """
+
 def electric_source(data, meta):
   """
     Fix up electricity for how much the Grids consume
+    @ In, data, dict, data requeset
+    @ In, meta, dict, state information
+    @ Out, data, dict, filled data
+    @ Out, meta, dict, state information
   """
   # flip sign because we consume the electricity
   E = -1.0 * meta['HERON']['activity']['electricity']

147399802cb14bc270f12c3357a71997295f2c59
Elizabeth K. Worsham <elizabeth.worsham@inl.gov>
2021-06-08 11:37:26 -0600

    compile user manual on windows with make_docs.bat

---
+++ doc/user_manual/make_docs.bat
@@ -0,0 +1,19 @@
+ECHO Starting to compile manual...
+chmod u+x script/copy_tex.sh
+script/copy_tex.sh
+python script/generate_user_manual.py
+cd build
+pdflatex -interaction=nonstopmode HERON_user_manual.tex
+cd ..
+bibtex build/HERON_user_manual
+cd build
+pdflatex -interaction=nonstopmode HERON_user_manual.tex
+pdflatex -interaction=nonstopmode HERON_user_manual.tex
+cd ..
+cp -f build/HERON_user_manual.pdf pdf/
+ECHO User manual build complete.
+ECHO Cleaning build
+cd ..
+rm -rf build
+ECHO Done
+

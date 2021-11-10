21a2866ec818870a9d7cfb07a6b3d69325bc1801
Paul W. Talbot <paul.talbot@inl.gov>
2021-11-09 17:31:28 -0700

    added synth signals to plots

--- src/DispatchPlot.py
+++ src/DispatchPlot.py
@@ -40,6 +40,7 @@ class DispatchPlot(PlotPlugin):
     specs.addSub(InputData.parameterInputFactory('source', contentType=InputTypes.StringType))
     specs.addSub(InputData.parameterInputFactory('macro_variable', contentType=InputTypes.StringType))
     specs.addSub(InputData.parameterInputFactory('micro_variable', contentType=InputTypes.StringType))
+    specs.addSub(InputData.parameterInputFactory('signals', contentType=InputTypes.StringListType))
     return specs
 
   def __init__(self):
@@ -54,6 +55,7 @@ class DispatchPlot(PlotPlugin):
     self._source = None
     self._macroName = None
     self._microName = None
+    self._addSignals = []
 
   def handleInput(self, spec):
     """
@@ -65,10 +67,12 @@ class DispatchPlot(PlotPlugin):
     for node in spec.subparts:
       if node.getName() == 'source':
         self._sourceName = node.value
-      if node.getName() == 'macro_variable':
+      elif node.getName() == 'macro_variable':
         self._macroName = node.value
-      if node.getName() == 'micro_variable':
+      elif node.getName() == 'micro_variable':
         self._microName = node.value
+      elif node.getName() == 'signals':
+        self._addSignals = node.value
 
   def initialize(self, stepEntities):
     """
@@ -116,19 +120,23 @@ class DispatchPlot(PlotPlugin):
     sample_ids = df[self._source.sampleTag].unique()
     cluster_ids = df['_ROM_Cluster'].unique()  # TODO: find way to not hardcode name
     macro_steps = df[self._macroName].unique()
+    total_axs = len(grouped_vars) + len(self._addSignals)
 
     for sample_id in sample_ids:
       for macro_step in macro_steps:
         for cluster_id in cluster_ids:
           fig = plt.figure()
+          axs = fig.subplots(total_axs, 1, sharex=True)
           # Filter data to plot correct values for current dimension
           dat = df[
             (df[self._source.sampleTag] == sample_id) &
             (df[self._macroName] == macro_step) &
             (df['_ROM_Cluster'] == cluster_id)
           ]
+          micro = dat[self._microName]
+          # add dispatch lines
           for i, (key, group) in enumerate(grouped_vars.items()):
-            ax = fig.add_subplot(len(grouped_vars),1,i+1)
+            ax = axs[i] #fig.add_subplot(total_axs, 1, i+1, sharex=True)
             for var in group:
               _, comp_name, tracker, resource = var.split('__')
               comp_label = comp_name.replace('_', ' ').title()
@@ -140,14 +148,26 @@ class DispatchPlot(PlotPlugin):
               if tracker == 'level':
                 style = ':'
               else:
-                style = '-.'
+                style = '-'
               # Plot the micro-step variable on the x-axis (i.e Time)
-              ax.plot(dat[self._microName], dat[var], linestyle=style, label=var_label)
+              ax.plot(micro, dat[var],  marker='.', linestyle=style, label=var_label)
               ax.set_title(key.title())
               ax.set_xlabel(self._microName.title())
               ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
           file_name = f"dispatch_id{sample_id}_y{macro_step}_c{cluster_id}.png"
+          # add signal lines (e.g. synthetic history values)
+          for s, name in enumerate(self._addSignals):
+            var = dat.get(name, None)
+            if var is None:
+              msg = f'Requested signal variable "{name}" but variable not in data!'
+              self.raiseAnError(msg)
+            ax = axs[i+s+1]#fig.add_subplot(total_axs, 1, i+s+2)
+            ax.plot(micro, var, marker='.', linestyle='-', label=name)
+            ax.set_title(name.title())
+            ax.set_xlabel(self._microName.title())
+            ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
           fig.tight_layout()
           fig.savefig(file_name)
           self.raiseAMessage(f'Saved figure to "{file_name}"')
           plt.clf()
+

d60bde1a398d4d2da5b753873ff8640bc1026bfd
Botros N. Hanna <botros.hanna@inl.gov>
2022-08-23 14:03:40 -0600

    fixing more typos

--- tests/integration_tests/mechanics/optimizedDispatch2Hybrid/create_user_input.py
+++ tests/integration_tests/mechanics/optimizedDispatch2Hybrid/create_user_input.py
@@ -3,18 +3,19 @@
 """
 A script to create a user-input file for the HYBRID user.
 
-This script is an initial step that the HYBRID user needs to create new names, if necessary, to: 1- The optimized dispatch outputs, 2- The components' capacities.
-This script extracts the optimized dispatch outputs from the dispatch prints csv file and the list of components (and their capacities) from the HERON input XML file.
+This script is an initial step that the HYBRID user needs to rename, if necessary, the optimized dispatch outputs and the components' capacities.
+This script extracts the optimized dispatch outputs from the dispatch prints CSV file and the list of components (and their capacities) from the HERON input XML file.
+
 
 It takes the following arguments:
 1- The HERON input XML file
-2- The optimized dispatch outputs csv file.
-For example, To run the script, use the following command:
+2- The optimized dispatch outputs CSV file.
+For example, to run the script, use the following command:
   python create_user_input.py heron_input.xml Debug_Run_o/dispatch_print.csv
-The output will be a user input file: "user_input.txt".
-The user needs to review the user input file and change/review the HYRBID variables and capacities there.
+The output will be a user input file: "user_input.txt"
+The user needs to review the user input file and change/review the HYBRID variables and capacities there.
 
-Next, the user can run the "export2Hybrid.py" which loads the dispatch outpus from HERON to a new file that HYBRID can use. The variables names in the autoloaded HYBRID file are borrowd from "user_input.txt"
+Next, the user can run the "export2Hybrid.py" which loads the dispatch outputs from HERON to a new file that HYBRID can use. The variables' names in the autoloaded HYBRID file are borrowed from "user_input.txt"
 """
 
 #####
@@ -32,9 +33,9 @@ import pandas as pd
 # Section 1: Extracting components' capacities Function
 def extract_capacities_heron_hybrid(heron_input, output_file):
   """
-    Creates a list of components' capacities from the HERON inpue file plus creating placeholders for the components' capacities in HYRBID
+    Creates a list of components' capacities from the HERON input file plus creating placeholders for the components' capacities in HYBRID
     @ In, heron input, str, the HERON input XML file
-    @ In, output_file, str, the text file where the list of components capacities should be printed.
+    @ In, output_file, str, the text file where the list of components' capacities should be printed.
     @ Out, None
   """
 
@@ -56,7 +57,7 @@ def extract_capacities_heron_hybrid(heron_input, output_file):
         + "\n" + "# Components' list is extracted from HERON input file: " + '"' + args.HERON_input_file + '"'
         + "\n" + "# The capacities are listed in the form:"
         + "\n" + "#" + "\t\t\t\t " + "HERON_component_capacity_1 = HYBRID_component_capacity_1" + "\n"
-        + "# The user is expected to change/review the HYRBID components' capacities." + "\n"
+        + "# The user is expected to change/review the HYBRID components' capacities." + "\n"
       )
       # Components' capacities for HYBRID are also printed as placeholders until the user change them
       for comp in component:
@@ -85,8 +86,8 @@ def extract_capacities_heron_hybrid(heron_input, output_file):
 # Section 2: Extracting dispatch outputs Function
 def extract_dispatches_heron_hybrid(dispatch_print, output_file):
   """
-    Creates a list of dispatches (components' optimized variables) from the HERON inpue file plus creating placeholders for the dispatches in HYRBID
-    @ In, dispatch_print, str, the distpach csv file with a list of optimized variables
+    Creates a list of dispatches (components' optimized variables) from the HERON input file plus creating placeholders for the dispatches in HYBRID
+    @ In, dispatch_print, str, the dispatch CSV file with a list of optimized variables
     @ In, output_file, str, the text file where the list of optimized dispatches should be printed.
     @ Out, None
   """
@@ -105,7 +106,7 @@ def extract_dispatches_heron_hybrid(dispatch_print, output_file):
       + '"' + args.HERON_dipatch_csv_file
       + '"' + "\n" + "# The variables are listed in the form:"
       + "\n" + "#" + "\t\t\t\t" + "HERON_variable_1 = HYBRID_variable_1"
-      + "\n" + "# The user is expected to change/review the HYRBID variables"
+      + "\n" + "# The user is expected to change/review the HYBRID variables"
       + "\n"
     )
 
@@ -130,7 +131,7 @@ if __name__ == "__main__":
   # Inputs from the user
   parser.add_argument("HERON_input_file", help="HERON XML input file path")
   parser.add_argument(
-    "HERON_dipatch_csv_file", help="HERON optimized dispatch output csv file"
+    "HERON_dipatch_csv_file", help="HERON optimized dispatch output CSV file"
   )
   args = parser.parse_args()
 

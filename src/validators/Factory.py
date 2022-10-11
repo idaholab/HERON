10b2698d9764fe81e830e1e108bde99c3d6df4de
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2022-10-11 16:11:47 -0600

    importing FARM.src is more reliable to check for FARM.

--- src/validators/Factory.py
+++ src/validators/Factory.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import _utils as hutils
 try:
-  import FARM
+  import FARM.src
   farm_loc = FARM.__path__
 except ModuleNotFoundError:
   try:

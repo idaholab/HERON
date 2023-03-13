48efba7100a152a9fda0c9cdfcc081f854d994f0
yangx789 <xingyue.yang@inl.gov>
2023-03-13 15:45:34 -0600

    Edited dispatch plot

--- src/DispatchPlot.py
+++ src/DispatchPlot.py
@@ -106,15 +106,6 @@ class DispatchPlot(PlotPlugin):
         gr[key] = [var]
     return gr
 
-  def align_yaxis(ax1, v1, ax2, v2):
-    """adjust ax2 ylimit so that v2 in ax2 is aligned to v1 in ax1"""
-    _, y1 = ax1.transData.transform((0, v1))
-    _, y2 = ax2.transData.transform((0, v2))
-    inv = ax2.transData.inverted()
-    _, dy = inv.transform((0, 0)) - inv.transform((0, y1-y2))
-    miny, maxy = ax2.get_ylim()
-    ax2.set_ylim(miny+dy, maxy+dy)
-
   def plot_component(self, fig, axes, df, grp_vars, comp_idx, sid, mstep, cid, cdict) -> None:
     """
       Plot and output the optimized dispatch for a specific sample, year, and cluster.

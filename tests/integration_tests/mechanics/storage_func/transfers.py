afa44409231a0f3af0e1ee3aa3c97a77dd0ab4d3
talbpw <paul.talbot@inl.gov>
2024-06-11 08:17:47 -0600

    implemented and tested custom user input for functions

--- tests/integration_tests/mechanics/storage_func/transfers.py
+++ tests/integration_tests/mechanics/storage_func/transfers.py
@@ -16,8 +16,18 @@ def flex_price(data, meta):
   """
   sine = meta['HERON']['RAVEN_vars']['Signal']
   t = meta['HERON']['time_index']
+  # check for the existince of the custom functions
+  for node in meta['HERON']['custom_input']:
+    if node.tag == 'General':
+      ans = node.find('TheAnswer').text
+      qus = node.find('TheQuestion').text
+    elif node.tag == 'scalar':
+      scalar = float(node.text)
+    elif node.tag == 'loc':
+      loc = float(node.text)
+  print(f'The answer to the question "{qus}" is "{ans}".')
   # DispatchManager
   # scale electricity consumed to flex between -1 and 1
-  amount = - 2 * (sine[t] - 0.5)
+  amount = scalar * (sine[t] + loc)
   data = {'reference_price': amount}
   return data, meta
